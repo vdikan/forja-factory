14e2fa1efc6f9d7470e22af3ea4cf5693c22df20
Vladimir Dikan <vdikan@vivaldi.net>
2021-07-21 15:47:12 +0200

    User interrupts handled, output of merged results (thermal)

--- apps/thermal/build.lisp
+++ apps/thermal/build.lisp
@@ -26,19 +26,33 @@
     (format out "]")))
 
 
+(defun finalize ()
+  (let* ((*print-right-margin* 24)
+         (results (merge-results)))
+    (with-open-file (out "output.lisp"
+                         :direction :output
+                         :if-exists :supersede)
+      (pprint-fill out results))
+    (results-edn results))
+  (if reset-main-after-run (reset-main)))
+
+
 (defun run-main-app ()
   (format t "Starting Thermal-Transport Workflow...~%")
   (load #P"config.lisp")
   (format t "Configuration file loaded~%")
-  (run-main)
-  (let* ((*print-right-margin* 24)
-         (results (merge-results)))
-   (with-open-file (out "output.lisp"
-                       :direction :output
-                       :if-exists :supersede)
-     (pprint-fill out results))
-   (results-edn results))
-  (if reset-main-after-run (reset-main))
+  (handler-case (run-main)
+    (#+sbcl sb-sys:interactive-interrupt
+     ;; #+ccl  ccl:interrupt-signal-condition
+     ;; #+clisp system::simple-interrupt-condition
+     ;; #+ecl ext:interactive-interrupt
+     ;; #+allegro excl:interrupt-signal
+     () (progn
+          (format *error-output* "User interrupt of the WF. Aborting.~&")
+          (finalize)
+          (uiop:quit)))
+    (error (c) (format t "Unknown error:~&~a~&" c)))
+  (finalize)
   (format t "Thermal-Transport Workflow finished!~%"))
 
 
